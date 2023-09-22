2b55e151860c1244ac39bf2a0d52c1724e40e319
Raoul Wograndl <90349386+RaoulWo@users.noreply.github.com>
2023-09-22 10:32:33 +0200

    Implement camera class

---
+++ OpenGL/camera.cpp
@@ -0,0 +1,98 @@
+#include "camera.h"
+
+Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch)
+    : front_(glm::vec3(0.0f, 0.0f, -1.0f)), move_speed_(kSpeed), mouse_sensitivity_(kSensitivity), zoom_(kZoom)
+{
+    position_ = position;
+    world_up_ = up;
+    yaw_ = yaw;
+    pitch_ = pitch;
+    
+    UpdateCameraVectors();
+}
+
+Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch)
+    : front_(glm::vec3(0.0f, 0.0f, -1.0f)), move_speed_(kSpeed), mouse_sensitivity_(kSensitivity), zoom_(kZoom)
+{
+    position_ = glm::vec3(posX, posY, posZ);
+    world_up_ = glm::vec3(upX, upY, upZ);
+    yaw_ = yaw;
+    pitch_ = pitch;
+
+    UpdateCameraVectors();
+}
+
+void Camera::ProcessKeyboard(CameraMovement direction, float deltaTime)
+{
+    float velocity = move_speed_ * deltaTime;
+
+    switch (direction)
+    {
+    case CameraMovement::kForward:
+        position_ += front_ * velocity;
+        break;
+    case CameraMovement::kBackward:
+        position_ -= front_ * velocity;
+        break;
+    case CameraMovement::kLeft:
+        position_ -= right_ * velocity;
+        break;
+    case CameraMovement::kRight:
+        position_ += right_ * velocity;
+        break;
+    }
+}
+
+void Camera::ProcessMouseMovement(float xOffset, float yOffset, GLboolean constrainPitch)
+{
+    xOffset *= mouse_sensitivity_;
+    yOffset *= mouse_sensitivity_;
+
+    yaw_ += xOffset;
+    pitch_ += yOffset;
+
+    if (constrainPitch)
+    {
+        if (pitch_ > 89.0f)
+        {
+            pitch_ = 89.0f;
+        }
+        if (pitch_ < -89.0f)
+        {
+            pitch_ = -89.0f;
+        }
+    }
+
+    UpdateCameraVectors();
+}
+
+void Camera::ProcessMouseScroll(float yOffset)
+{
+    zoom_ -= yOffset;
+    if (zoom_ < 1.0f)
+    {
+        zoom_ = 1.0f;
+    }
+    if (zoom_ > 45.0f)
+    {
+        zoom_ = 45.0f;
+    }
+}
+
+glm::mat4 Camera::view_matrix() const
+{
+    return glm::mat4();
+}
+
+void Camera::UpdateCameraVectors()
+{
+    // Calculate the new front vector.
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw_)) * cos(glm::radians(pitch_));
+    front.y = sin(glm::radians(pitch_));
+    front.z = sin(glm::radians(yaw_)) * cos(glm::radians(pitch_));
+    front_ = glm::normalize(front);
+    // Calculate the new right and up vectors.
+    right_ = glm::normalize(glm::cross(front_, world_up_));
+    up_ = glm::normalize(glm::cross(right_, front_));
+}
---
+++ OpenGL/camera.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+
+
+enum CameraMovement
+{
+    kForward,
+    kBackward,
+    kLeft,
+    kRight
+};
+
+const float kYaw = -90.0f;
+const float kPitch = 0.0f;
+const float kSpeed = 2.5f;
+const float kSensitivity = 0.1f;
+const float kZoom = 45.0f;
+
+class Camera
+{
+public:
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = kYaw, float pitch = kPitch);
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch);
+
+    void ProcessKeyboard(CameraMovement direction, float deltaTime);
+    void ProcessMouseMovement(float xOffset, float yOffset, GLboolean constrainPitch = true);
+    void ProcessMouseScroll(float yOffset);
+
+    glm::mat4 view_matrix() const;
+
+private:
+    void UpdateCameraVectors();
+
+    glm::vec3 position_;
+    glm::vec3 front_;
+    glm::vec3 up_;
+    glm::vec3 right_;
+    glm::vec3 world_up_;
+
+    float yaw_;
+    float pitch_;
+    float move_speed_;
+    float mouse_sensitivity_;
+    float zoom_;
+};
+
